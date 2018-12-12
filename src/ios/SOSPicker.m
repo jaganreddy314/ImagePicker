0f98911dea58ad5e68e1278206f5368f74b3e533
jagan.thathagari <jagan.thathagari@simpro.co>
2018-12-12 14:02:00 +1000

    changing the place where its creating directory

--- src/ios/SOSPicker.m
+++ src/ios/SOSPicker.m
@@ -175,7 +175,11 @@ typedef enum : NSUInteger {
 
     NSMutableArray * result_all = [[NSMutableArray alloc] init];
     int i = 1;
-    
+    NSString* filePath;
+    NSFileManager* fileMgr = [[NSFileManager alloc] init];
+    //Clean or move the temp file after invoked this plugin when not in use, transfering the file into persistent.
+    NSString* docsPath = [NSTemporaryDirectory()stringByStandardizingPath];
+                
     CDVPluginResult* result = nil;
 
     for (GMFetchItem *item in fetchArray) {
@@ -183,6 +187,10 @@ typedef enum : NSUInteger {
         if ( !item.image_fullsize ) { 
             continue;
         }
+
+        do {
+            filePath = [NSString stringWithFormat:@"%@/%@%03d.%@", docsPath, CDV_PHOTO_PREFIX, i++, @"jpg"];
+        } while ([fileMgr fileExistsAtPath:filePath]);
         
         // no scaling, no downsampling, this is the fastest option
         if (self.width == 0 && self.height == 0 && self.quality == 100 && self.outputType != BASE64_STRING) {
@@ -238,13 +246,7 @@ typedef enum : NSUInteger {
                 [result_all addObject:[destData base64EncodedStringWithOptions:0]];
             }else{
                 //Write into file
-                NSString* filePath;
-                NSFileManager* fileMgr = [[NSFileManager alloc] init];
-                //Clean or move the temp file after invoked this plugin when not in use, transfering the file into persistent.
-                NSString* docsPath = [NSTemporaryDirectory()stringByStandardizingPath];
-                do {
-                    filePath = [NSString stringWithFormat:@"%@/%@%03d.%@", docsPath, CDV_PHOTO_PREFIX, i++, @"jpg"];
-                } while ([fileMgr fileExistsAtPath:filePath]);
+                
                 
                 if (![destData writeToFile:filePath options:NSAtomicWrite error:&err]) {
                     result = [CDVPluginResult resultWithStatus:CDVCommandStatus_IO_EXCEPTION messageAsString:[err localizedDescription]];
