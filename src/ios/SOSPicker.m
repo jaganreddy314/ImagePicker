3fb66faf62f71ac403853a1caf4326e777c33a31
AddOn <>
2018-12-13 11:24:18 +1000

    debugging filename

--- src/ios/SOSPicker.m
+++ src/ios/SOSPicker.m
@@ -205,10 +205,10 @@ typedef enum : NSUInteger {
                 NSDateFormatter *dateFormatter=[[NSDateFormatter alloc] init]; 
                 [dateFormatter setDateFormat:@"%f%@"];
                 // or @"yyyy-MM-dd hh:mm:ss a" if you prefer the time with AM/PM 
-                NSLog(@"%@",[dateFormatter stringFromDate:[NSDate date]]);
+                NSLog(@"%@", [dateFormatter stringFromDate:[NSDate date]]);
                 NSDate *date = [NSDate date];
-                NSString ti = (long)[date timeIntervalSince1970];
-                NSLog(@"ti%@",ti);
+                NSTimeInterval ti = [date timeIntervalSince1970];
+                NSLog(@"ti %@", ti);
                 
                 //timeSince = [NSString stringWithFormat:@”%f”,[[NSDate date] timeIntervalSince1970] * 1000];
                 filePath = [NSString stringWithFormat:@"%@/%@%03d.%@", docsPath, CDV_PHOTO_PREFIX, i++, @"jpg"];
